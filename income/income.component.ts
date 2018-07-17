1eff6599a22b349cf50e7ce5b6e36a883084e804
thenetlog <emaildotmm@gmail.com>
2018-07-17 21:09:21 +0630

    folder

---
+++ income/income.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+// interface myData {
+//   data: Object;
+//   key: string;
+//   value: string;
+// }
+
+@Component({
+  selector: 'app-income',
+  templateUrl: './income.component.html',
+  styleUrls: ['./income.component.scss']
+})
+
+export class IncomeComponent implements OnInit {
+
+  incomes$: any = [];
+
+  constructor(private data: DataService) {
+  
+   }
+
+  // ngOnInit() {
+  //   this.data.getIncomes().subscribe(
+  //     data =>  {this.incomes = data;
+  //     this.incomes = Array.of(this.incomes);
+  //     }
+  //   );
+  // }
+  ngOnInit() {
+    this.data.getIncomes().subscribe(
+      (data: any) => {this.incomes$ = data.data;}
+    );
+  }
+}
