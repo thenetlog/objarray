6a57667b6f043dad0815ab3fdd227c569ab12ac4
thenetlog <emaildotmm@gmail.com>
2018-07-17 21:12:53 +0630

    folder

---
+++ expense/expense.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-expense',
+  templateUrl: './expense.component.html',
+  styleUrls: ['./expense.component.scss']
+})
+export class ExpenseComponent implements OnInit {
+
+  expenses$: any = [];
+
+  constructor(private data: DataService) { }
+
+  ngOnInit(): void {
+    this.data.getExpense().subscribe(
+      (data: any) => {this.expenses$ = data.data;}
+    );
+  }
+    
+
+}
