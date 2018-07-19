37ea13adcec9f8084759ad90d61eee9c04179caa
thenetlog <emaildotmm@gmail.com>
2018-07-19 21:30:43 +0630

    Income Component

--- income/income.component.ts
+++ income/income.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
+import { Income } from '../income';
 
 @Component({
   selector: 'app-income',
@@ -11,12 +12,21 @@ export class IncomeComponent implements OnInit {
 
   incomes$: any = [];
 
-  constructor(private data: DataService) {
-   }
+  @Input() income: Income;
+  selectedIncome: any = {};
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getIncomes().subscribe(
-      (data: any) => {this.incomes$ = data.data;}
+      (data: any) => {this.incomes$ = data.data;},
+      err => console.error(err),
+      () => console.log('done loading'),
     );
   }
+
+  onSelect(inc: Income) {
+    this.selectedIncome = inc;
+  }
+
 }
