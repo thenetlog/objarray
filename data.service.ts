81c605d7b656d180f2dd2a37f8658edaeaec8b51
thenetlog <emaildotmm@gmail.com>
2018-07-19 21:16:41 +0630

    Service file

--- data.service.ts
+++ data.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http'
 import { Observable, of } from 'rxjs';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';  
-import 'rxjs/add/operator/catch';  
-import 'rxjs/add/observable/throw';  
-//import { filter, map } from 'rxjs/operators';
+import { catchError, map, tap, filter} from 'rxjs/operators';
+import { Income } from './income';
 
 interface myData {
   data: Object;
 }
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  private headers: HttpHeaders;
+  readonly ROOT_URL = 'http://localhost:5000';
+  
+
+  constructor(private http: HttpClient) { 
+    this.headers = new HttpHeaders({'Content-Type': 'application/json' });
+  }
+
+  upIncome(Select) {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Access-Control-Allow-Headers': '*' })
+  };
+    const body = new FormData();
+    body.append('token', 'ZG90bmV0QVBJ');
+    body.append('objInfo', JSON.stringify(Select));
+    return this.http.post(this.ROOT_URL + '/api/income/SaveIncome', body, httpOptions);
+  }''
 
   getIncomes() {
-    return this.http.get<myData>('http://localhost:5000/api/income/GetIncome/0');
+    return this.http.get<myData>(this.ROOT_URL + 'api/income/GetIncome/0');
   }
 
   getExpense() {
-    return this.http.get<myData>('http://localhost:5000/api/expense/GetExpense/0');
+    return this.http.get<myData>(this.ROOT_URL + '/api/expense/GetExpense/0');
   }
+
+  
 
 }
