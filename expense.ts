67e6f3302d1f596c0e0c5400306ec9bdb8729cbe
thenetlog <emaildotmm@gmail.com>
2018-07-20 23:28:15 +0630

    Expense Class/Model

---
+++ expense.ts
@@ -0,0 +1,8 @@
+export class Expense {
+    ExpenseID: number = 0;
+    TransactionDate: Date = null;
+    Particular: string = '';
+    Amount: number = 0;
+    Created_date: Date = null;
+    Modified_date: Date = null;
+}
