e2fa7e564c8f709c7de1773dfc5f500b51684487
thenetlog <emaildotmm@gmail.com>
2018-07-20 23:21:29 +0630

    Income Component

--- income/income.component.ts
+++ income/income.component.ts
@@ -11,12 +11,26 @@ import { Income } from '../income';
 export class IncomeComponent implements OnInit {
 
   incomes$: any = [];
+  //for toggle()
+  public Update: boolean = false;
+  public incomef: any = 'Update';
 
   @Input() income: Income;
   selectedIncome: any = {};
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {
+   }
 
+   //for income edit form
+   toggle() {
+    this.Update = !this.Update;
+    if(this.Update)  
+      this.incomef = "Hide Form";
+    else
+      this.incomef = "Update";
+  }
+
+  //Read
   ngOnInit() {
     this.data.getIncomes().subscribe(
       (data: any) => {this.incomes$ = data.data;},
@@ -25,8 +39,15 @@ export class IncomeComponent implements OnInit {
     );
   }
 
+  //select
   onSelect(inc: Income) {
     this.selectedIncome = inc;
+  }
+
+  //Create
+  incomeModel = new Income();
+  createIncome() {
+    this.data.storeIncome(this.incomeModel).subscribe(val => console.log(val));
   }
 
 }
